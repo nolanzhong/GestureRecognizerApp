0232b779901282af74a127b3e3f84e297a557703
nolanzhong <nolan.zhong@duke.edu>
2023-01-02 01:29:25 -0500

    completed gesture recognizer

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // allowing for the image to be clickable
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+    }
+    @objc func changePic() {
+        if myLabel.text == "Optimus Prime" {
+            imageView.image = UIImage(named: "1498134404-transformers-dark-of-the-moon-bumblebee-poster")
+            myLabel.text = "Bumblebee"
+        } else {
+            imageView.image = UIImage(named: "optimus-prime-is-like-a-huge-figure")
+            myLabel.text = "Optimus Prime"
+        }
     }
 
 
